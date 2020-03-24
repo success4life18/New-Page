850c28e3a13a97fdfd9a4bb5440b3da1be3afacd
Success <success4life18@gmail.com>
2020-03-24 15:00:20 +0100

    added a js file

---
+++ script.js
@@ -0,0 +1,28 @@
+let fName = "Success";
+    lName = "Ndubuisi";
+    fullName = fName + " " + lName;
+
+ console.log(fullName);
+
+ let courses = ["Html", "Css", "Js",];
+ for (let i=0; i < courses.length; i++){
+
+    console.log(courses[i] );
+ }
+
+    
+
+ 
+ const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%3===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
