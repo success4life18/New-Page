a17deb26422e2b67910696c37b161220c7990348
success4life18 <53913570+success4life18@users.noreply.github.com>
2020-04-19 12:45:54 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function NumberReplacer(maxNumber = 100) {
+    let minNumber = 0
+    let numberRange = [...Array(maxNumber).keys()].map(n => n + 1)
+    let output = [];
+    numberRange.forEach((number) => {
+        let input = ''
+        if (number % 2 === 0) {
+            input = 'yu'
+        }
+        if (number % 3 === 0) {
+            input += input ? '-gi' : 'gi'
+        }
+        if (number % 5 === 0) {
+            input += input ? '-oh' : 'oh'
+        }
+        if (!input) {
+            input = number
+        }
+        output.push(input)
+    });
+    console.log(output);
+    return output;
+
+     
+};
+NumberReplacer(100);
+NumberReplacer(80);
